ad67b3e9f8c4d119e78d78440aae4cbc9e13dbb4
unknown <adh.yogas@gmail.com>
2025-05-12 22:40:46 +0700

    update 120525

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./website/**/*.{html,js,py}",
+    "./templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
